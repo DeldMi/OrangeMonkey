fe51905de106558be81cf781bab8e349ec57fb11
DeldMi <andre88444@gmail.com>
2024-09-30 20:28:38 -0300

    up

--- printDescompricaText/main.user.js
+++ printDescompricaText/main.user.js
@@ -2,18 +2,41 @@
 // @name         Imprimir texto da aula da Descomplica
 // @namespace    http://andre.a3gs.com.br
 // @homepage     http://andre.a3gs.com.br
-// @version      1.10
+// @version      1.13
 // @date         2024-09-30
-// @description  Add print button to open article in a new window with CSS for printing, using MutationObserver.
 // @author       André Felipe
+// @description  Add print button to open article in a new window with CSS for printing, using MutationObserver.
 // @match        *://aulas.descomplica.com.br/*
-// @homepageURL https://raw.githubusercontent.com/DeldMi/impimirDescomprica/refs/heads/main/main.user.js
-// @updateURL    https://raw.githubusercontent.com/DeldMi/impimirDescomprica/refs/heads/main/tampermonkey/print-article.meta.js
-// @downloadURL  https://raw.githubusercontent.com/DeldMi/impimirDescomprica/refs/heads/main/tampermonkey/print-article.user.js
-// @icon         https://sf-helper.com/img/icon_16.png
-// @icon64       https://sf-helper.com/img/icon_64.png
-// @run-at       document-end
-// @grant        none
+// @icon         https://aulas.descomplica.com.br/graduacao/img/Logo_D.svg
+// @homepageURL  https://raw.githubusercontent.com/DeldMi/OrangeMonkey/refs/heads/main/printDescompricaText/main.user.js
+// @updateURL    https://raw.githubusercontent.com/DeldMi/OrangeMonkey/refs/heads/main/printDescompricaText/tampermonkey/print-article.meta.js
+// @downloadURL  https://raw.githubusercontent.com/DeldMi/OrangeMonkey/refs/heads/main/printDescompricaText/tampermonkey/print-article.user.js
+// @include     http://*
+// @include     https://*
+// @run-at      document-end
+// @grant       GM_listValues
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_deleteValue
+// @grant       GM_xmlhttpRequest
+// @grant       GM_info
+// @grant       GM_openInTab
+// @grant       GM_setClipboard
+// @grant       GM_registerMenuCommand
+// @grant       GM_unregisterMenuCommand
+// @grant       GM_notification
+// @grant       GM_evalFunction
+// @grant       GM_download
+// @grant       GM.info
+// @grant       GM.listValues
+// @grant       GM.setValue
+// @grant       GM.getValue
+// @grant       GM.deleteValue
+// @grant       GM.openInTab
+// @grant       GM.setClipboard
+// @grant       GM.xmlHttpRequest
+// @connect     aulas.descomplica.com.br
+// @connect     descomplica.com.br
 // ==/UserScript==
 
 // Configuração3
